7b001fe925d8841d63a83d74e47fa78dc3b4ec9c
Nick de Beer <alu.nic@gmail.com>
2021-04-03 03:38:56 +0200

    init

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# serum-copy
-🌌 Install all serum presets in one go ⚡
+# serum-copy ✨
+Ease of install for serum pack installation 
+
+# how to run
+```
+./Serum-Copy -PacksFolder "C:\downloads\Serum Packs"
+```
+
+### parameters
+-`PacksFolder` = where all your downloaded serum packs exist <br>
+-`SerumFolder` = (optional parameters) where the packs will be installed to. Defaults to *"C:\Users\USER_ACCOUNT\Documents\Xfer\Serum Presets"*
+
---
+++ Serum-Copy.ps1
@@ -0,0 +1,107 @@
+﻿param (
+        [Parameter(Mandatory = $true, ValueFromPipeline, Position = 0)]
+        [string]$PacksFolder, # F:\Downloads\Completed\Presets\Serum
+
+        [Parameter(Mandatory = $false, ValueFromPipeline, Position = 1)]
+        [string]$SerumFolder = (Join-Path $env:USERPROFILE "Documents\Xfer\Serum Presets")
+)
+
+Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Unrestricted -Force;
+
+# Global Configuration
+$MaxDirectoryDepthSearch = 5
+
+$noisesDirectoryMatchFilter = @('Serum Noises','Noises','noise')
+$lfoDirectoryMatchFilter = @('LFO','','')
+$presetsDirectoryMatchFilter = @('Serum Presets','Presets','preset')
+$tablesDirectoryMatchFilter = @('Serum Tables','Tables','table')
+
+
+
+
+function Copy-Packs
+{
+    param (
+        [string]$PacksFolder,
+        [string[]]$Filters
+    )
+
+        # get all folders for the serum packs
+        $packs = Get-PackNames -PacksFolder $PacksFolder -Filter $filter
+        foreach ($pack in $packs) 
+        {
+            # path of a single pack
+            $packPath = (Join-Path $SerumFolder $pack)
+            foreach ($filter in $Filters) 
+            {
+                
+            }
+        }
+}
+
+function Get-PackNames
+{
+    param (
+        [string]$PacksFolder,
+        [string]$Filter
+    )
+
+    return Get-ChildItem -Path $PacksFolder -Filter $Filter -Recurse -Directory -Depth 0 | Select-Object -Property Name
+}
+
+function Get-PresetsFolder
+{
+    param (
+        [string]$PacksFolder,
+        [string]$Filter
+    )
+
+
+    return Get-ChildItem -Path $PacksFolder -Filter $Filter -Recurse -Directory
+}
+
+function Get-NoisesFolder
+{
+    param (
+        [string]$PackFolder,
+        [string]$Filter
+    )
+
+    $noiseFolderFoundFlag = $false
+    $directoryDepth = 0
+    while ($noiseFolderFoundFlag -eq $false -and $directoryDepth -le $MaxDirectoryDepthSearch) {
+        try {
+            Get-ChildItem -Path $PackFolder -Filter $Filter -Recurse -Directory -Depth $directoryDepth
+        } 
+        finally {
+            ++$directoryDepth
+        }
+    }
+
+    return Get-ChildItem -Path $PacksFolder -Filter $Filter -Recurse -Directory -Depth 0
+}
+
+
+Traverse -Depth 0 -> 5
+
+
+
+
+
+# LFO Shapes - Create Single Level Folder for Reach Pack
+#.shp
+
+# Noises
+#.wav .aif
+
+# Presets
+#.fxp
+
+# Skins
+
+# System
+
+# Tables
+#.wav
+
+# Copy-Packs -PacksFolder $PacksFolder
